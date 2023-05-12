c62695bb50ef2d96d457e2cf112f36464f2438f8
Gabriel Cordova Medeiros <gabiceemi@gmail.com>
2023-05-12 09:57:44 -0300

    concluded

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -7,9 +7,14 @@ interface IRequest {
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
-
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin || !user) {
+      throw new Error("User not already exists!");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
